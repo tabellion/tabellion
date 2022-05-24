343b2230d5ec4c951f6cbcda631241bf8a0b97d5
David-Baron <davidbaron@mycodingsoft.com>
2022-05-24 17:47:18 +0200

    Fix include paths

--- ajax/majeure_patro.php
+++ ajax/majeure_patro.php
@@ -4,27 +4,23 @@
 // Licence Publique Générale GPL GNU publiée par la Free Software Foundation
 // Texte de la licence : http://www.gnu.org/copyleft/gpl.html
 //-------------------------------------------------------------------
-require_once('../Commun/config.php');
-require_once('../Commun/constantes.php');
-require_once('../Commun/ConnexionBD.php');
+require_once __DIR__ . '/../Commun/config.php';
+require_once __DIR__ . '/../Commun/constantes.php';
+require_once __DIR__ . '/../Commun/ConnexionBD.php';
 
 $a_resultats = array();
-if (isset ($_GET['term']))
-{
-  $st_rech = substr(trim($_GET['term']),0,30);
-  $st_rech=utf8_decode($st_rech);  
-  $connexionBD = ConnexionBD::singleton($gst_serveur_bd,$gst_utilisateur_bd,$gst_mdp_utilisateur_bd,$gst_nom_bd);
-  $st_requete = "select idf_groupe,patronyme from variantes_patro where patronyme COLLATE latin1_german1_ci like  '$st_rech%' and majeure=1 order by patronyme";
-  $a_majeures = $connexionBD->liste_valeur_par_clef($st_requete);
-  $a_resultats = array();
-  foreach ($a_majeures as $i_idf_groupe => $st_patronyme)
-  {
-     $a_ligne =array();
-     $a_ligne['text'] = utf8_encode($st_patronyme);
-     $a_ligne['id'] = utf8_encode($i_idf_groupe);      
-     $a_resultats[] = $a_ligne;     
-  }
+if (isset($_GET['term'])) {
+    $st_rech = substr(trim($_GET['term']), 0, 30);
+    $st_rech = utf8_decode($st_rech);
+    $connexionBD = ConnexionBD::singleton($gst_serveur_bd, $gst_utilisateur_bd, $gst_mdp_utilisateur_bd, $gst_nom_bd);
+    $st_requete = "select idf_groupe,patronyme from variantes_patro where patronyme COLLATE latin1_german1_ci like  '$st_rech%' and majeure=1 order by patronyme";
+    $a_majeures = $connexionBD->liste_valeur_par_clef($st_requete);
+    $a_resultats = array();
+    foreach ($a_majeures as $i_idf_groupe => $st_patronyme) {
+        $a_ligne = array();
+        $a_ligne['text'] = utf8_encode($st_patronyme);
+        $a_ligne['id'] = utf8_encode($i_idf_groupe);
+        $a_resultats[] = $a_ligne;
+    }
 }
-echo json_encode($a_resultats); 
-
-?>
+echo json_encode($a_resultats);
